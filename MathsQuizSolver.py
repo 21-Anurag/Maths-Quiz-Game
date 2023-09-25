4bbe1a0a567abc268aed7e9bcc635e8c8af43e01
Anurag Mukherjee <132687550+21-Anurag@users.noreply.github.com>
2023-09-25 21:38:51 +0530

    Add files via upload

---
+++ MathsQuizSolver.py
@@ -0,0 +1,36 @@
+import random
+import time
+
+OPERATORS =["+","-","*"]
+MIN_OPERAND = 0
+MAX_OPERAND = 2000000
+TOTAL_PROBLEMS = int(input("Number of Problems to be generated: "))
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND,MAX_OPERAND)
+    right = random.randint(MIN_OPERAND,MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+    
+    exp = str(left) + " " + operator + " " + str(right)
+    answer = eval(exp)
+    
+    return exp, answer
+
+wrong = 0
+input("Press enter to start!")
+print("---------------------------")
+
+start_time = time.time()
+for i in range(TOTAL_PROBLEMS):
+    exp , answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i+1)+": " + exp + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time,2)
+     
+print("---------------------------")
+print("Nice Work!! You finished in ",total_time," second!")
